32534331e1dedff0bcab398db4c504d7c80bc123
brayan <bemq1605@gmail.com>
2021-02-22 22:18:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  HashRouter as Router ,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import About from "./rutas/About"
+import Appp from "./rutas/appp"
+import Faq from "./rutas/Faq"
+import Home from "./rutas/Home"
+import Store from "./rutas/Store"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Store">Store</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li>
+              <Link to="/Appp">App</Link>
+            </li>
+            <li>
+              <Link to="/Fap">FAQ</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/About">
+              <About />
+            </Route>
+            <Route path="/Appp">
+              <Appp />
+            </Route>
+            <Route path="/Fap">
+              <Faq />
+            </Route>
+            <Route path="/Store">
+              <Store />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
     </div>
   );
 }
